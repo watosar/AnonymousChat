47d5abc854c2b0909abf751b060af2d1e29ea725
watosar <38435692+watosar@users.noreply.github.com>
2019-03-08 11:51:42 +0900

    Update channel.py

--- AnonymousChat/channel.py
+++ AnonymousChat/channel.py
@@ -1,32 +1,40 @@
-import aiohttp
+# -*- coding: utf-8 -*-
 
-headers = {'User-Agent': 'DiscordBot', 'Content-Type': 'application/json'}
+from aiohttp import request as aiohttp_request
 
 
 class AnoncChannel:
-  __slots__ = ('_core', 'anonc_member', 'anonc_role', 'anonc_webhook')
-  
-  def __init__(self, channel, anonc_webhook, anonc_member=None, anonc_role=None):
-    self._core = channel
-    self.anonc_webhook = anonc_webhook
-    member_id, role_id = anonc_webhook.name.split(':')
-    self.anonc_member = anonc_member or channel.guild.get_member(int(member_id, 36))
-    self.anonc_role = anonc_role or channel.guild.get_role(int(role_id, 36))
-
-  def __getattr__(self, key):
-    if key not in self.__slots__:
-      return getattr(self._core, key)
-    else:
-      return getattr(self, key)
-  
-  def __eq__(self, other):
-    return self.core == other
+    """
+    to manage AnonChat member | chat_id | role | message sending
+    """
     
-  def is_equal_to(self, channel):
-    return self.__eq__(channel)
-
-  async def send(self, anonc_message):
-    async with aiohttp.ClientSession(headers=headers) as session:
-        async with session.post(self.anonc_webhook.url, json=anonc_message.to_dict(self)):
+    __slots__ = ('_core', 'anonc_webhook', 'anonc_member', 'anonc_id_role', 'anonc_id')
+    
+    def __init__(self, channel, anonc_webhook, anonc_member=None, anonc_id_role=None, anonc_id=None):
+        self._core = channel
+        self.anonc_webhook = anonc_webhook
+        self.anonc_member = anonc_member or channel.guild.get_member(int(anonc_webhook.name))
+        self.anonc_id_role = anonc_id_role
+        self.anonc_id = channel.topic
+    
+    def __getattr__(self, key):
+        if key in self.__slots__:
+            return getattr(self, key)
+        else:
+            return getattr(self._core, key)
+      
+    def __eq__(self, other):
+        return self._core == other
+    
+    def is_equal_to(self, channel):
+        return self.__eq__(channel)
+    
+    async def anonc_send(self, anonc_message):
+        headers = {'User-Agent': 'DiscordBot', 'Content-Type': 'application/json'}
+        async with aiohttp_request('POST', self.anonc_webhook.url, headers=headers, json=anonc_message.to_dict(self)) as session:
             return
-
+    
+    async def edit(self, **kwargs):
+        if 'topic' in kwargs:
+            self.anonc_id = kwargs['topick']
+        await self._core.edit(**kwargs)
