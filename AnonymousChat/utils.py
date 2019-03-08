28de0139d0497c82c543944c583a33f7519eedc5
watosar <38435692+watosar@users.noreply.github.com>
2019-03-08 11:53:45 +0900

    Update utils.py

--- AnonymousChat/utils.py
+++ AnonymousChat/utils.py
@@ -1,13 +1,19 @@
+# -*- coding: utf-8 -*-
+
 from math import log, ceil
+from datetime import timezone, timedelta
 
 letters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+timezone = timezone(timedelta(hours=9))
 
 
-# random_strings = lambda len_: ''.join(__import__('random').choice(letters) for _ in range(len_))
-def random_string(len_):
+def random_string(len_:int) -> str:
     return ''.join(__import__('random').choice(letters[10:]) for _ in range(len_))
 
 
-def base36encode(num):
+def base36encode(num:int) -> str:
   return ''.join(reversed([letters[digit] for num in (num,) for _ in range(ceil(log(num,36))) for num, digit in ((num//36,num%36),)]))
 
+
+def jst(timestamp):
+    return timestamp.astimezone(timezone)
